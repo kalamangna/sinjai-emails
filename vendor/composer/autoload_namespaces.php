3bc6aa34d080eca7852db58a3dd8e6f3a0659fe7
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2026-06-11 12:30:14 +0800

    Optimize composer dependencies (no-dev)

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'org\\bovigo\\vfs\\' => array($vendorDir . '/mikey179/vfsstream/src/main/php'),
     'Smalot\\PdfParser\\' => array($vendorDir . '/smalot/pdfparser/src'),
 );
